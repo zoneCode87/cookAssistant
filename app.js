4d98119c4b9bb7c8507628a7549234a5785eafbe
zoneCode87 <zonecode48785@gmail.com>
2025-05-30 21:50:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express()
 const port = 3000
 const routerAPi = require('./router/api');
 app.use('/api',routerAPi);
-app.use(express.static('public'))
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "pug");
 app.use(express.json());
 
